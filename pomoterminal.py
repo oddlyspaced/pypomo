d0b3f81a5a4b93a6e872ec8d7d144d2d13ac44af
oddlyspaced <oddlyspaced1@gmail.com>
2022-04-15 09:41:40 +0530

    pomoterminal: draw digits first

--- pomoterminal.py
+++ pomoterminal.py
@@ -13,15 +13,15 @@ curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
 w, h = stdscr.getmaxyx()
 w -= 1
 h -= 1
-for i in range(h):
-    stdscr.addstr(w - 1, h - i, "=")
-
 def draw_digit(y, x):
     for i in range(len(digit)):
         stdscr.addstr(y + i, x, digit[i])
 
 draw_digit(0, 0)
-draw_digit(0, 8)
+draw_digit(0, 7)
+
+for i in range(h):
+    stdscr.addstr(w - 1, h - i, "=")
 
 stdscr.refresh()
 stdscr.getch()
