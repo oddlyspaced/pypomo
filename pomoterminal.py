2ff965b8479e86d1f015d3e65ad6f99a6bdb162f
oddlyspaced <oddlyspaced1@gmail.com>
2022-04-15 09:12:56 +0530

    pomoterminal: refactor and remove wrapper method

--- pomoterminal.py
+++ pomoterminal.py
@@ -1,16 +1,14 @@
 import os
 import curses
 
-def draw_timer(stdscr):
-    stdscr.clear()
-    stdscr.refresh()
-    curses.start_color()
+stdscr = curses.initscr()
 
-    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-    h, w = stdscr.getmaxyx()
-    stdscr.addstr(0, 0, "Hello World", curses.color_pair(1))
-    stdscr.refresh()
-    stdscr.getch()
+stdscr.clear()
+stdscr.refresh()
+curses.start_color()
 
-
-curses.wrapper(draw_timer)
+curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+h, w = stdscr.getmaxyx()
+stdscr.addstr(0, 0, "Hello World", curses.color_pair(1))
+stdscr.refresh()
+stdscr.getch()
