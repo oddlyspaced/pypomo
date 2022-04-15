d6a5c2f4b66751b9f91a8f473373f79c0e7655e5
oddlyspaced <oddlyspaced1@gmail.com>
2022-04-15 09:10:58 +0530

    pomoterminal: setup simple hellow world curses script

--- pomoterminal.py
+++ pomoterminal.py
@@ -1,5 +1,16 @@
 import os
+import curses
 
-columns, rows = os.get_terminal_size()
-print(columns)
-print(rows)
+def draw_timer(stdscr):
+    stdscr.clear()
+    stdscr.refresh()
+    curses.start_color()
+
+    curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    h, w = stdscr.getmaxyx()
+    stdscr.addstr(0, 0, "Hello World", curses.color_pair(1))
+    stdscr.refresh()
+    stdscr.getch()
+
+
+curses.wrapper(draw_timer)
