c227480c5cf2b93081e7a783b19427be3358a2e4
oddlyspaced <oddlyspaced1@gmail.com>
2022-04-15 09:24:35 +0530

    pomoterminal: draw line at bottom

--- pomoterminal.py
+++ pomoterminal.py
@@ -8,7 +8,10 @@ stdscr.refresh()
 curses.start_color()
 
 curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLACK)
-h, w = stdscr.getmaxyx()
-stdscr.addstr(0, 0, "Hello World", curses.color_pair(1))
+w, h = stdscr.getmaxyx()
+w -= 1
+h -= 1
+for i in range(h):
+    stdscr.addstr(w - 1, h - i, "=")
 stdscr.refresh()
 stdscr.getch()
